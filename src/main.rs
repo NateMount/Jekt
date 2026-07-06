04107f9f9a88d5402219e0db0a0c0e5af97bcfb0
Lgsx <ngmount2021@gmail.com>
2026-07-06 13:00:52 -0400

    Updated execution with argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 // [Jekt:Main]
 // Author(s): Nate Mount (@xxi)
 
+// === Imports
+mod utils;
+mod functions;
+use crate::utils::{argparse::RunMode, display_help};
+
 fn main(){
 
+    let mode: RunMode = utils::argparse::build_run_config();
+
+    match mode {
+        RunMode::HELP => display_help(),
+        RunMode::LIST => functions::list(),
+        RunMode::INFO(project_id) => println!("INFO::{}", project_id),
+        RunMode::NEW(project_id, path) => println!("NEW::{} @ {}", project_id, path),
+        RunMode::PATH(project_id) => println!("PATH::{}", project_id),
+        RunMode::DELETE(project_id) => println!("DELETE::{}", project_id),
+        RunMode::CONFIG(setting, value) => println!("CONFIG::{}={}", setting, value)
+    }
 }
