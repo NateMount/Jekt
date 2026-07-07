3cc63049e17e8ad2303faaa3a1448f5558ca5d7c
Lgsx <ngmount2021@gmail.com>
2026-07-07 11:15:21 -0400

    Removed  from

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -12,7 +12,6 @@ pub fn display_help() {
 \t\x1b[1;4;32mcommand\x1b\t\t\x1b[4;34margs\x1b[0m\t\t\t\x1b[4mdescritpion\x1b[0m
 \t\x1b[1;32mhelp\x1b[0m\t\t\t\t\tshow this message
 \t\x1b[1;32mlist\x1b[0m\t\t\t\t\tlist all indexed projects
-\t\x1b[1;32mconfig\x1b[0m\t\t\x1b[34m(\x1b[0m setting, value \x1b[34m)\x1b[0m\tset value of \x1b[1;34m`setting`\x1b[0m to \x1b[1;34m`value`\x1b[0m
 \t\x1b[1;32minfo\x1b[0m\t\t\x1b[34m(\x1b[0m projectId \x1b[34m)\x1b[0m\t\tshow metadata and info on \x1b[1;34m`projectId`\x1b[0m
 \t\x1b[1;32mpath\x1b[0m\t\t\x1b[34m(\x1b[0m projectId \x1b[34m)\x1b[0m\t\tshow full path to \x1b[1;34m`projectId`\x1b[0m
 \t\x1b[1;32mnew\x1b[0m\t\t\x1b[34m(\x1b[0m projectId, path \x1b[34m)\x1b[0m\tbuild a new project at \x1b[1;34m`path`\x1b[0m with id \x1b[1;34m`projectId`\x1b[0m
