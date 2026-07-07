9a280630cf14d4999bdb294d5552b0de909473f7
Lgsx <ngmount2021@gmail.com>
2026-07-07 09:15:47 -0400

    Moved index loading functionality to  function for ease of use

--- src/commands.rs
+++ src/commands.rs
@@ -16,7 +16,9 @@ const CONFIG_PATH: &str = "./resources/jekt-conf.toml";
 struct Project {
     id: String,
     desc: String,
-    stack: Vec<String>
+    stack: Vec<String>,
+    start_date: toml::value::Datetime,
+    path: String
 }
 
 #[derive(Debug, Deserialize)]
@@ -24,19 +26,23 @@ struct ProjectIndex {
     project: Vec<Project>
 }
 
-// === Command Functions
+// === Functions
 
-pub fn list(){
-
+fn load_projects() -> ProjectIndex {
     let index_data = fs::read_to_string(INDEX_PATH)
     .expect(&format!("\x1b[1;31m[!]\x1b[0m Jekt-Index file cannot be accessed \x1b[3;34m@{}\x1b[0m\n", INDEX_PATH));
 
-    let index: ProjectIndex = toml::from_str(&index_data)
-    .expect("\x1b[1;31m[!]\x1b[0m Jekt-Index file cannot be parsed\n");
+    toml::from_str(&index_data)
+    .expect("\x1b[1;31m[!]\x1b[0m Jekt-Index file cannot be parsed\n")
+}
 
-    println!("\t\x1b[1;4;34mProjectId\x1b[0m\t\x1b[1;4mDescription\x1b[0m\t\t\x1b[1;4;35mStack\x1b[0m");
+// === Command Functions
+
+pub fn list(){
+    let index: ProjectIndex = load_projects();
+
+    println!("\t\x1b[1;4;34mProjectId\x1b[0m\t\x1b[1;4mDescription\x1b[0m");
     for project in index.project {
-        println!(">>\t\x1b[1;34m[\x1b[0m {} \x1b[1;34m]\x1b[0m\t{}\t\x1b[1;35m{:?}\x1b[0m", project.id, project.desc, project.stack);
+        println!(">>\t\x1b[1;34m[\x1b[0m {} \x1b[1;34m]\x1b[0m\t\x1b[3m{}\x1b[0m", project.id, project.desc);
     }
-
 }
