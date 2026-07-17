c31900a87d21dc3369eeeb821aaa38e10e237400
Lgsx <ngmount2021@gmail.com>
2026-07-17 10:42:16 -0400

    Added documentation

--- src/utils/fileops.rs
+++ src/utils/fileops.rs
@@ -8,6 +8,14 @@ use std::io::Write;
 
 // === Functions
 
+/// Used to load contents of a toml index file
+/// 
+/// **Params**
+/// - `index` : Path of index to load
+/// 
+/// **Considerations**
+/// - If toml cannot be parsed an empty `ProjectIndex` is returned
+/// - If index path cannot be opened an error is returned
 pub fn load_source(index: &str) -> ProjectIndex {
     let index_data = fs::read_to_string(index)
     .expect(&format!("\x1b[1;31m[!]\x1b[0m Jekt-Index file cannot be accessed \x1b[3;34m@{}\x1b[0m\n", index));
@@ -18,6 +26,11 @@ pub fn load_source(index: &str) -> ProjectIndex {
     }
 }
 
+/// Writes `ProjectIndex` to index file
+/// 
+/// **Params**
+/// - `projects` : List of projects to be written to index file
+/// - `index` : Path of index to write to
 pub fn write_project(projects: Vec<Project>, index: &str) -> Result<(), std::io::Error> {
     let mut write_out = fs::File::options().append(true).create(true).open(index)?;
 
@@ -27,11 +40,13 @@ pub fn write_project(projects: Vec<Project>, index: &str) -> Result<(), std::io:
             "\n[[project]]\n{}\n", 
             toml::to_string(&project).expect("\x1b[1;31m[!]\x1b[0m Unable to generate TOML formatted project")
         )?;
-    }
-
-    Ok(())
+    } Ok(())
 }
 
+/// Used to completely clear a file
+/// 
+/// **Params**
+/// - `source` : Path of file to clear
 pub fn blank_source(source: &str){
     match fs::File::create(source){
         Ok(_) => println!("\x1b[1;33m[%]\x1b[0m Saving updated index"),
